6e4c944d5d0f1a87b4098e17e3e402099137b468
nacho <nacho@DESKTOP-D8OR4S4>
2016-11-03 11:33:49 +0000

    Kata4 Versión 3

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,21 +1,17 @@
 package kata4;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Kata4 {
 
-    public static void main(String[] args) {
-        File file = new File("c:\\");
-        print(file.listFiles()," ");
-    }
+    public static void main(String[] args) throws IOException {
+        String fileName = "emailsfilev1.txt";
+        ArrayList<String> mailList = MailListReader.read(fileName);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
+     }
     
-    private static void print(File[] files, String indent){
-        if(files == null) return;
-        for (File file : files) {
-            System.out.println(indent + (file.isDirectory() ? "+" : "-") 
-                    + file.getName());
-            if(!file.isDirectory() || file.isHidden()) continue;
-            print(file.listFiles(), indent + " ");
-        }
-    }
 }
